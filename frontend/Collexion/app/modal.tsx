cf641c6adbe0a65c466d2ac08a22edecd7abf4bd
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-01 12:17:16 +0100

    x

--- frontend/Collexion/app/modal.tsx
+++ frontend/Collexion/app/modal.tsx
@@ -7,7 +7,7 @@ import { ThemedView } from '@/components/themed-view';
 export default function ModalScreen() {
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a Modal</ThemedText>
+      <ThemedText type="title">This is a modal</ThemedText>
       <Link href="/" dismissTo style={styles.link}>
         <ThemedText type="link">Go to Home screen</ThemedText>
       </Link>
