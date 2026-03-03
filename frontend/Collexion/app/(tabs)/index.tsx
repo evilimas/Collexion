dc22383058e892b34b12fccc04147be7d5e181ca
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-03 13:11:29 +0100

    index side started

--- frontend/Collexion/app/(tabs)/index.tsx
+++ frontend/Collexion/app/(tabs)/index.tsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import React from 'react';
 
-const index = () => {
+import backgroundImage from '@/assets/images/background.avif';
+
+const app = () => {
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={backgroundImage}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Collexion</Text>
+      </ImageBackground>
     </View>
   );
 };
 
-export default index;
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    textShadowColor: 'black',
+    textShadowOffset: { width: 1, height: 4 },
+    textShadowRadius: 6,
+    // flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
