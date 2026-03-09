3fc1c9fae277b458e79451eb593eba7e6e94a4b9
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-09 20:43:18 +0100

    new handlehlds tab

---
+++ frontend/Collexion/app/(tabs)/handhelds.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Handhelds = () => {
+  return (
+    <View>
+      <Text>Handhelds</Text>
+    </View>
+  );
+};
+
+export default Handhelds;
