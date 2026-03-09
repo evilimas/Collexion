778e09d4d56c50af5ad11f49188df19c3ac8f870
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-09 20:43:34 +0100

    new controllers tab

---
+++ frontend/Collexion/app/(tabs)/controllers.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Controllers = () => {
+  return (
+    <View>
+      <Text>Controllers</Text>
+    </View>
+  );
+};
+
+export default Controllers;
