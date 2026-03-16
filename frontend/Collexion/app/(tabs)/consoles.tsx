8adbf13cfc855b80aff13925a3dc025bc807e33b
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-03-16 13:37:03 +0100

    consoles tab update

--- frontend/Collexion/app/(tabs)/consoles.tsx
+++ frontend/Collexion/app/(tabs)/consoles.tsx
@@ -1,12 +1,89 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TextInput,
+} from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 const Consoles = () => {
   return (
-    <View>
-      <Text>Consoles</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('@/assets/images/background3.jpg')}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.text}>Consoles</Text>
+        <TextInput
+          placeholder="Search Consoles"
+          placeholderTextColor="rgba(255, 255, 255, 0.7)"
+          style={styles.searchInput}
+        />
+      </ImageBackground>
     </View>
   );
 };
 
 export default Consoles;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 10,
+    flex: 1,
+  },
+
+  image: {
+    width: '100%',
+    height: '100%',
+    textShadowColor: 'black',
+    textShadowOffset: { width: 1, height: 4 },
+    textShadowRadius: 6,
+    resizeMode: 'cover',
+  },
+  text: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  linkContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingHorizontal: 20,
+  },
+  link: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderRadius: 8,
+    borderColor: 'rgba(255, 255, 255, 0.5)',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  searchContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  searchInput: {
+    color: 'white',
+    fontSize: 15,
+    backgroundColor: 'rgba(15, 15, 15, 0.5)',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginHorizontal: 5,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    borderRadius: 15,
+    marginBottom: 10,
+    // width: '100%',
+  },
+});
