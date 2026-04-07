567afffebb75cf00661673310066372a76c713e8
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-04-07 14:20:21 +0200

    x

---
+++ frontend/Collexion/components/Console.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Console = () => {
+  return (
+    <View>
+      <Text>Console</Text>
+    </View>
+  );
+};
+
+export default Console;
---
+++ frontend/Collexion/components/Consoles.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Consoles = () => {
+  return (
+    <View>
+      <Text>Consoles</Text>
+    </View>
+  );
+};
+
+export default Consoles;
