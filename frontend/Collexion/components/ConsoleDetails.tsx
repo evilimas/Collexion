4149db74b5b99bfe593c82d7d2b533377688d8fc
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-05-11 13:40:23 +0200

    new component

---
+++ frontend/Collexion/components/ConsoleDetails.tsx
@@ -0,0 +1,37 @@
+import { View, Text, ImageSourcePropType } from 'react-native';
+import React from 'react';
+
+type Props = {
+  name: string;
+  model?: string;
+  edition?: string;
+  color: string;
+  condition: 'Mint' | 'Like New' | 'Good' | 'Fair';
+  picture?: ImageSourcePropType;
+};
+
+const ConsoleDetails = ({
+  name,
+  model,
+  edition,
+  color,
+  condition,
+  picture,
+}: Props) => {
+  return (
+    <View>
+      {/* {picture ? (
+        <Image source={picture} resizeMode="contain" />
+      ) : null} */}
+      <View >
+        <Text >{name}</Text>
+        {model ? <Text >Model: {model}</Text> : null}
+        {edition ? <Text >Edition: {edition}</Text> : null}
+        <Text >Color: {color}</Text>
+        <Text >Condition: {condition}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ConsoleDetails;
