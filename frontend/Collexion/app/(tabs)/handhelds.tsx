0b777d7bf2d6a91701433884b5b66ff4e496b05b
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-06-19 13:55:07 +0200

    fix

--- frontend/Collexion/app/(tabs)/handhelds.tsx
+++ frontend/Collexion/app/(tabs)/handhelds.tsx
@@ -12,7 +12,7 @@ import { Link } from 'expo-router';
 import { collection, CollectionItem } from '@/data/newData';
 
 const Handhelds = () => {
-  const [handheld, setHandhelds] = useState<CollectionItem[]>([]);
+  const [handhelds, setHandhelds] = useState<CollectionItem[]>([]);
 
   useEffect(() => {
     const filteredHandhelds = collection.filter(
