79c730a8c287212823a3221e1b209116e72b6fec
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-08-17 15:01:28 +0200

    x

--- frontend/Collexion/app/(tabs)/add-item.tsx
+++ frontend/Collexion/app/(tabs)/add-item.tsx
@@ -355,7 +355,7 @@ const AddItem = () => {
               placeholderTextColor="rgba(255, 255, 255, 0.7)"
               style={styles.descriptionInputStyle}
               multiline={true}
-              numberOfLines={6}
+              numberOfLines={5}
               value={description}
               onChangeText={setDescription}
             />
