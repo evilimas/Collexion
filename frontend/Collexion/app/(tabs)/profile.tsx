d2ec32ef9054c318dac83f4e25d607585baed49f
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-08-20 14:15:27 +0200

    profile tab update

--- frontend/Collexion/app/(tabs)/profile.tsx
+++ frontend/Collexion/app/(tabs)/profile.tsx
@@ -3,15 +3,35 @@ import {
   Text,
   StyleSheet,
   ImageBackground,
-  Image,
-  TextInput,
   ScrollView,
+  Pressable,
+  Image,
 } from 'react-native';
-import { useEffect, useState } from 'react';
+import { useClerk, useUser } from '@clerk/expo';
 
-const userName = 'User';
+const Logout = () => {
+  const { signOut } = useClerk();
+
+  const handleLogout = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
+
+  return (
+    <Pressable onPress={handleLogout}>
+      <Text style={styles.logout}>Logout</Text>
+    </Pressable>
+  );
+};
 
 const Profile = () => {
+  const { user } = useUser();
+  const userName = user?.firstName || 'UserName';
+  const userEmail =
+    user?.primaryEmailAddress?.emailAddress || 'user@example.com';
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -22,20 +42,57 @@ const Profile = () => {
         <View style={styles.overlay}>
           <Text style={styles.text}>Profile</Text>
           <ScrollView>
-            <View>
-              <Text style={styles.link}>Hi {userName}</Text>
+            <View style={styles.heading}>
+              <View style={styles.headingLeft}>
+                <Image
+                  source={
+                    user?.imageUrl
+                      ? { uri: user.imageUrl }
+                      : require('@/assets/images/user.png')
+                  }
+                  style={{ width: 100, height: 100, borderRadius: 50 }}
+                />
+              </View>
+              <View style={styles.headingRight}>
+                <Text style={styles.textHeader}>{userName}</Text>
+                <Text style={styles.textHeaderemail}>{userEmail}</Text>
+                <Text
+                  style={[
+                    styles.textHeaderemail,
+                    { marginBottom: 10, fontSize: 14 },
+                  ]}
+                >
+                  Member since: {user?.createdAt?.toDateString()}
+                </Text>
+                <Text style={styles.textHeaderemail}>
+                  0 Items in collection
+                </Text>
+              </View>
             </View>
             <View>
-              <Text style={styles.link}>Edit Profile</Text>
-              <Text style={styles.link}>Change Password</Text>
-              <Text style={styles.link}>Privacy Settings</Text>
-              <Text style={styles.link}>Notification Settings</Text>
-              <Text style={styles.link}>Connected Accounts</Text>
-              <Text style={styles.link}>Payment Methods</Text>
-              <Text style={styles.link}>Order History</Text>
-              <Text style={styles.link}>Wishlist</Text>
-              <Text style={styles.link}>Saved Items</Text>
-              <Text style={styles.link}>Logout</Text>
+              <Text style={styles.link}>
+                Edit Profile <Text>&gt;</Text>
+              </Text>
+              <Text style={styles.link}>
+                Change Password <Text>&gt;</Text>
+              </Text>
+              <Text style={styles.link}>
+                Privacy Settings <Text>&gt;</Text>
+              </Text>
+              <Text style={styles.link}>
+                Notification Settings <Text>&gt;</Text>
+              </Text>
+              <Text style={styles.link}>
+                Connected Accounts <Text>&gt;</Text>
+              </Text>
+
+              <Text style={styles.link}>
+                Wishlist <Text>&gt;</Text>
+              </Text>
+              <Text style={styles.link}>
+                Saved Items <Text>&gt;</Text>
+              </Text>
+              <Logout />
             </View>
           </ScrollView>
         </View>
@@ -70,6 +127,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 20,
   },
+  textHeader: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  textHeaderemail: {
+    color: 'white',
+    fontSize: 16,
+    marginBottom: 0,
+  },
   linkContainer: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     paddingHorizontal: 20,
@@ -85,6 +153,8 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(255, 255, 255, 0.5)',
     borderWidth: 1,
     marginBottom: 10,
+    display: 'flex',
+    justifyContent: 'space-between',
   },
   searchContainer: {
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -102,5 +172,37 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     borderRadius: 15,
     marginBottom: 10,
+  },
+  heading: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    gap: 10,
+    borderRadius: 8,
+    borderColor: 'rgba(255, 255, 255, 0.5)',
+    borderWidth: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+  },
+  headingLeft: {},
+  headingRight: {},
+  logout: {
+    color: 'rgb(247, 0, 0)',
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    borderRadius: 8,
+    borderColor: 'rgba(255, 255, 255, 0.5)',
+    borderWidth: 1,
+    marginBottom: 10,
+    backgroundColor: 'rgba(168, 4, 4, 0.5)',
+    width: '100%',
+    textAlign: 'center',
   },
 });
