8482d7799ee859ed51e25834fd0b2985071e036f
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-09-04 08:35:33 +0200

    x

--- frontend/Collexion/components/hello-wave.tsx
+++ frontend/Collexion/components/hello-wave.tsx
@@ -4,7 +4,7 @@ export function HelloWave() {
   return (
     <Animated.Text
       style={{
-        fontSize: 27,
+        fontSize: 28,
         lineHeight: 32,
         marginTop: -6,
         animationName: {
