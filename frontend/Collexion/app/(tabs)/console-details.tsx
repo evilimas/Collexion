491073f3a3f9200cc7c8b655c17b7fda536dd2c9
Egidijus Vilimas <eg.vilimas@gmail.com>
2026-09-14 08:21:22 +0200

    styling update

--- frontend/Collexion/app/(tabs)/console-details.tsx
+++ frontend/Collexion/app/(tabs)/console-details.tsx
@@ -7,6 +7,7 @@ import {
   Pressable,
   Alert,
 } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { deleteItem } from '@/hooks/use-collection-items';
 
@@ -28,7 +29,6 @@ const ConsoleDetail = () => {
   const withBox = params.withBox === 'true'; // Convert string to boolean
   const from = (params.from as string) || '/consoles';
 
-
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -43,53 +43,100 @@ const ConsoleDetail = () => {
           >
             <Text style={styles.backText}>← Back</Text>
           </Pressable>
-
-          <View style={styles.detailCard}>
+          <View style={styles.pictureContainer}>
             {url ? (
               <Image
                 source={{ uri: url }}
                 style={styles.detailImage}
-                resizeMode="contain"
+                // resizeMode="cover"
               />
             ) : null}
-            <Text style={styles.detailName}>{name}</Text>
-            {model && <Text style={styles.detailInfo}>Model: {model}</Text>}
-            {edition && (
-              <Text style={styles.detailInfo}>Edition: {edition}</Text>
-            )}
-            <Text style={styles.detailInfo}>Color: {color}</Text>
-            <Text style={styles.detailInfo}>Condition: {condition}</Text>
-            <Text style={styles.detailInfo}>Manufacturer: {manufacturer}</Text>
-            <Text style={styles.detailInfo}>Description: {description}</Text>
-            <Text style={styles.detailInfo}>
-              Reshell: {reshell ? 'Yes' : 'No'}
-            </Text>
-            <Text style={styles.detailInfo}>
-              With Box: {withBox ? 'Yes' : 'No'}
-            </Text>
-            <View style={styles.editButtons}>
-              <Pressable style={styles.editBtn}>
-                <Text style={styles.editText}>Edit Item</Text>
-              </Pressable>
-              <Pressable
-                style={styles.editBtn}
-                onPress={async () => {
-                  try {
-                    await deleteItem(itemId);
-                    router.replace(from as any);
-                  } catch (error) {
-                    Alert.alert(
-                      'Unable to delete item',
-                      error instanceof Error
-                        ? error.message
-                        : 'Please try again.',
-                    );
-                  }
-                }}
-              >
-                <Text style={styles.editText}>Delete Item</Text>
-              </Pressable>
+          </View>
+
+          <Text style={styles.detailName}>{name}</Text>
+          {edition && <Text style={styles.detailEdition}> {edition}</Text>}
+          <View style={styles.detailCard}>
+            <View style={styles.detailRow}>
+              {model && (
+                <>
+                  <Text style={styles.detailInfo}>Model:</Text>
+                  <Text style={styles.detailInfo}>{model}</Text>
+                </>
+              )}
             </View>
+            <View style={styles.detailRow}>
+              {color && (
+                <>
+                  <Text style={styles.detailInfo}>Color:</Text>
+                  <Text style={styles.detailInfo}>{color}</Text>
+                </>
+              )}
+            </View>
+
+            <View style={styles.detailRow}>
+              {condition && (
+                <>
+                  <Text style={styles.detailInfo}>Condition:</Text>
+                  <Text style={styles.detailInfo}>{condition}</Text>
+                </>
+              )}
+            </View>
+            <View style={styles.detailRow}>
+              {manufacturer && (
+                <>
+                  <Text style={styles.detailInfo}>Manufacturer:</Text>
+                  <Text style={styles.detailInfo}>{manufacturer}</Text>
+                </>
+              )}
+            </View>
+
+            <View style={styles.detailRow}>
+              <Text style={styles.detailInfo}>Reshelled:</Text>
+              <Text style={styles.detailInfo}>{reshell ? 'Yes' : 'No'}</Text>
+            </View>
+            <View style={styles.detailLastRow}>
+              <Text style={styles.detailInfo}>With Box:</Text>
+              <Text style={styles.detailInfo}>{withBox ? 'Yes' : 'No'}</Text>
+            </View>
+          </View>
+          <View style={styles.detailCard}>
+            <View style={styles.descriptionContainer}>
+              {description && (
+                <>
+                  <Text style={styles.detailInfo}>Description:</Text>
+                  <Text style={styles.detailInfo}>{description}</Text>
+                </>
+              )}
+            </View>
+          </View>
+          <View style={styles.editButtons}>
+            <Pressable style={styles.editBtn}>
+              <View style={styles.actionContent}>
+                <MaterialIcons name="edit" size={22} color="white" />
+                <Text style={styles.editText}>Edit Item</Text>
+              </View>
+            </Pressable>
+            <Pressable
+              style={styles.deleteBtn}
+              onPress={async () => {
+                try {
+                  await deleteItem(itemId);
+                  router.replace(from as any);
+                } catch (error) {
+                  Alert.alert(
+                    'Unable to delete item',
+                    error instanceof Error
+                      ? error.message
+                      : 'Please try again.',
+                  );
+                }
+              }}
+            >
+              <View style={styles.actionContent}>
+                <MaterialIcons name="delete-outline" size={22} color="red" />
+                <Text style={styles.deleteText}>Delete Item</Text>
+              </View>
+            </Pressable>
           </View>
         </View>
       </ImageBackground>
@@ -110,7 +157,7 @@ const styles = StyleSheet.create({
   },
   overlay: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, 0.25)',
+    backgroundColor: 'rgba(0, 0, 0, 0.49)',
     paddingHorizontal: 10,
     paddingTop: 20,
   },
@@ -128,11 +175,18 @@ const styles = StyleSheet.create({
 
   editButtons: {
     display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-end',
+    gap: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'flex-end',
-    marginTop: 20,
+    marginTop: 'auto',
     color: 'white',
+  },
+
+  actionContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 6,
   },
 
   backText: {
@@ -146,6 +200,8 @@ const styles = StyleSheet.create({
     fontWeight: '600',
   },
   editBtn: {
+    flex: 1,
+    textAlign: 'center',
     paddingHorizontal: 12,
     paddingVertical: 6,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
@@ -154,28 +210,74 @@ const styles = StyleSheet.create({
     borderColor: 'rgba(255, 255, 255, 0.5)',
     marginBottom: 10,
   },
+  deleteText: {
+    color: 'rgb(255, 34, 34)',
+    fontSize: 16,
+    fontWeight: '600',
+  },
+  deleteBtn: {
+    flex: 1,
+    paddingHorizontal: 12,
+    paddingVertical: 6,
+    backgroundColor: 'rgba(192, 2, 2, 0.24)',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: 'rgba(255, 39, 39, 0.5)',
+    marginBottom: 10,
+  },
   detailCard: {
     backgroundColor: 'rgba(0, 0, 0, 0.55)',
     borderRadius: 12,
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.15)',
-    padding: 20,
+    padding: 12,
+    marginBottom: 10,
   },
   detailImage: {
     width: '100%',
-    height: 200,
-    borderRadius: 8,
+    height: 250,
+    resizeMode: 'cover',
     marginBottom: 16,
+    borderRadius: 8,
+  },
+  pictureContainer: {
+    alignItems: 'center',
+    borderRadius: 22,
   },
   detailName: {
     color: 'white',
     fontSize: 24,
     fontWeight: '700',
-    marginBottom: 16,
+    marginLeft: 4,
   },
   detailInfo: {
-    color: 'rgba(255, 255, 255, 0.85)',
+    color: 'rgb(255, 255, 255)',
     fontSize: 16,
-    marginTop: 8,
+    fontWeight: '600',
+    marginTop: 0,
   },
+  detailEdition: {
+    color: 'rgba(255, 255, 255, 0.69)',
+    fontSize: 17,
+    fontWeight: '600',
+    padding: 0,
+    marginBottom: 16,
+  },
+  detailRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 4,
+    marginBottom: 6,
+    borderBottomColor: 'rgba(255, 255, 255, 0.15)',
+    borderBottomWidth: 2,
+  },
+  detailLastRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    paddingVertical: 2,
+  },
+  descriptionContainer: {},
 });
